79d4cd32395c8a539ce4a18b0f5486483bea432c
BryanCI <clvn.intel@gmail.com>
2020-04-14 01:16:55 +0300

    testing

--- src/estimator.js
+++ src/estimator.js
@@ -1,84 +1,104 @@
-const covid19ImpactEstimator = (data) => {
-    /**
-     * This is a function to discard the decimal part of the computations
-     */
-    const floor = (value) => Math.floor(value);
-
-    // function to calculate the multiplication factor 
-    const calculateDays = (periodType, timeToElapse) => {
-        let numberOfDays;
-        if (periodType === "months") {
-            return numberOfDays = timeToElapse * 30;
-        }
-        else if (periodType === "weeks") {
-            return numberOfDays = timeToElapse * 7;
-        }
-        else {
-            return numberOfDays = timeToElapse;
-        }
+// eslint-disable-next-line consistent-return
+const calculateDays = (period, numberOfDays) => {
+    /*
+    * A function that receives the period type and integer count
+    * and returns the total number of days
+    * */
+  
+    if (period === 'days') {
+      return numberOfDays;
+    }
+    if (period === 'weeks') {
+      return numberOfDays * 7;
+    }
+    if (period === 'months') {
+      return numberOfDays * 30;
+    }
+  };
+  
+  const floor = (value) => {
+    if (value < 0) {
+      return Math.floor(value) + 1;
+    }
+    return Math.floor(value);
+  };
+  const covid19ImpactEstimator = (data) => {
+    /*
+    * Challenge 1
+    * */
+  
+    // Output
+    const output = {
+      data, // the input data you got
+      impact: {}, // your best case estimation
+      severeImpact: {} // your severe case estimation
     };
-
-    /**
-     * Challenge 1
-     */
-    const input = data;
-    const outPut = {
-        data: input,
-        impact: {},
-        severeImpact: {}
-    };
-
-    let mild = outPut.impact;
-    let severe = outPut.severeImpact;
-
-    //currently infected people
-    mild.currentlyInfected = floor(input.reportedCases * 10);
-
-    //severe impact inffected people
-    severe.currentlyInfected = floor(input.reportedCases * 50);
-
-    //method to calculate the factor
-    const factor = 2 * (numberOfDays / 3);
-
-    //infected people by requested time 
-    mild.infectionsByRequestedTime = floor(mild.currentlyInfected * factor);
-
-    //severely infected people by time elapsed
-    severe.infectionsByRequestedTime = floor(outPut.currentlyInfected * factor);
-
-    /**
-     * Challenge 2
-     */
-
-    //estimated number of severe cases that will need hospitalisation to recover 
-    mild.severeCasesByRequestedTime = floor(mild.infectionsByRequestedTime * 0.15);
-    severe.severeCasesByRequestedTime = floor(severe.infectionsByRequestedTime * 0.15);
-
-    //only 35 percent of hospital beds can be available for covid patients
-    const presentBeds = floor(input.totalHospitalBeds * 0.35);
-
-    mild.hospitalBedsByRequestedTime = (presentBeds - mild.severeCasesByRequestedTime);
-    severe.hospitalBedsByRequestedTime = (presentBeds - severe.severeCasesByRequestedTime);
-
-    /**
-     * Challenge 3
-     */
-
-    //5% of the infections by requested time that will require ICU 
-    mild.casesForICUByRequestedTime = floor(mild.infectionsByRequestedTime * 0.05);
-    severe.casesForICUByRequestedTime = floor(severe.infectionsByRequestedTime * 0.05);
-
-    //2% of the infections by request time that will need ventilators
-    mild.casesForVentilatorsByRequestedTime = floor(mild.infectionsByRequestedTime * 0.02);
-    severe.casesForVentilatorsByRequestedTime = floor(severe.infectionsByRequestedTime * 0.02);
-
-    //estimated amoumt of money the econmy is lossing per day
-    const dollarsLost = input.avgDailyIncome * input.avgDailyIncomePopulation;
-
-    mild.dollarsInFlight = floor((mild.infectionsByRequestedTime * dollarsLost) / numberOfDays);
-    severe.dollarsInFlight = floor((severe.infectionsByRequestedTime * dollarsLost) / numberOfDays);
-
-    return outPut;
-};
-
-export default covid19ImpactEstimator;
+  
+  
+    // Derive currently infected people
+    output.impact.currentlyInfected = Math.floor(data.reportedCases * 10);
+  
+    // Derive currently infected people for the severely infected
+    output.severeImpact.currentlyInfected = Math.floor(data.reportedCases * 50);
+  
+    // Calculate Number of days
+    const numberOfDays = calculateDays(data.periodType, data.timeToElapse);
+  
+    // Calculate Multiplication Factor
+    const factor = Math.floor(numberOfDays / 3);
+  
+    // Estimate the number of infected people x days from now
+    output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * (2 ** factor);
+  
+    // Estimate the number of severely infected people x days from now
+    const infectionsByRequestedTime = output.severeImpact.currentlyInfected * (2 ** factor);
+    output.severeImpact.infectionsByRequestedTime = infectionsByRequestedTime;
+  
+    /*
+    * Challenge 2
+    * */
+  
+    // Estimated number of severe positive cases that will require hospitalization to recover.
+    const value1 = floor(0.15 * output.impact.infectionsByRequestedTime);
+    const value2 = floor(0.15 * output.severeImpact.infectionsByRequestedTime);
+    output.impact.severeCasesByRequestedTime = value1;
+    output.severeImpact.severeCasesByRequestedTime = value2;
+  
+    // 35% of hospital beds are available for COVID-19 patients
+    const availableBeds = floor(0.35 * data.totalHospitalBeds);
+  
+    output.impact.hospitalBedsByRequestedTime = floor(availableBeds - value1);
+    output.severeImpact.hospitalBedsByRequestedTime = floor(availableBeds - value2);
+  
+    /*
+    * Challenge 3
+    * */
+  
+    // Estimated number of severe positive cases that will require ICU care.
+    const x1 = 0.05 * output.impact.infectionsByRequestedTime;
+    const x2 = 0.05 * output.severeImpact.infectionsByRequestedTime;
+    output.impact.casesForICUByRequestedTime = x1;
+    output.severeImpact.casesForICUByRequestedTime = x2;
+  
+    // Estimated number of severe positive cases that will require ventilators.
+    const y1 = 0.02 * output.impact.infectionsByRequestedTime;
+    const y2 = 0.02 * output.severeImpact.infectionsByRequestedTime;
+    output.impact.casesForVentilatorsByRequestedTime = floor(y1);
+    output.severeImpact.casesForVentilatorsByRequestedTime = floor(y2);
+  
+    // Estimate how much money the economy is likely to lose daily, over the said period of time.
+    const z1 = output.impact.infectionsByRequestedTime;
+    const z2 = output.severeImpact.infectionsByRequestedTime;
+    const dailyPop = data.region.avgDailyIncomePopulation;
+    const dailyIncome = data.region.avgDailyIncomeInUSD;
+  
+  
+    output.impact.dollarsInFlight = floor((z1 * dailyPop * dailyIncome) / numberOfDays);
+  
+    output.severeImpact.dollarsInFlight = floor((z2 * dailyPop * dailyIncome) / numberOfDays);
+  
+    // Return output object
+    return output;
+  };
+  
+  export default covid19ImpactEstimator;
